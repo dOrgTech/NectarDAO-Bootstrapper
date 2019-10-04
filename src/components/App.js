8a9a0cb7a718256175e4dbf1e8d256e3c876b88e
John Kelleher <johnpkelleher@gmail.com>
2019-10-04 18:36:06 -0400

    Header component styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,15 @@
-// TODO:
-// <Notifications
-//   <Header />
-//   <InitDialog onInitialized={setInitialized} />
-//   {initialized ?
-//     <ReputationBootstrap /> :
-//     <></>
-//   }
-// </Notifications>
-
 import React, { Component } from 'react'
 import {
   HashRouter
 } from 'react-router-dom'
+import '../App.scss'
+import Header from './shell/Header'
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div>
-          hello world
-        </div>
+        <Header />
       </HashRouter>
     )
   }
--- src/components/shell/Header.js
+++ src/components/shell/Header.js
@@ -1,1 +1,77 @@
 // TODO: EthFinex logo center, user's eth blockie
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid var(--border);
+  height: 159px;
+`
+
+const Title = styled.div`
+  color: var(--white-text);
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  margin: 20px 0px;
+  letter-spacing: 1px;
+`
+
+const NavWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const ActiveButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--white-text);
+  border: 1px solid var(--active-border);
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  padding: 18px 0px;
+  width: 156px;
+`
+
+const InactiveButton = styled(ActiveButton)`
+  border: 1px solid var(--inactive-border);
+  color: var(--inactive-text);
+`
+
+const InactiveButtonRight = styled(InactiveButton)`
+  border-left: none;
+`
+
+const InactiveButtonLeft = styled(InactiveButton)`
+  border-right: none;
+`
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Title>How do you want to earn Reputation for the NectarDAO?</Title>
+      <NavWrapper>
+        <ActiveButton>
+          <div>Lock NEC</div>
+        </ActiveButton>
+        <InactiveButtonRight>
+          <div>Airdrop</div>
+        </InactiveButtonRight>
+        <InactiveButtonRight>
+          <div>Bid GEN</div>
+        </InactiveButtonRight>
+      </NavWrapper>
+    </HeaderWrapper>
+  )
+}
+
+export default Header
