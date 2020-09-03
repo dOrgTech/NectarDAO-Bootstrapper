7d071cec1c2f222f142673d6296be13f562c4b01
unknown <bennett_noah11@yahoo.com>
2020-09-03 04:08:22 -0400

    governance page button and title

--- src/components/common/LogoAndText.js
+++ src/components/common/LogoAndText.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const LogoAndTextWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  color: var(--inactive-text);
+  color: #E2A907;
   line-height: 24px;
 `
 
--- src/components/pages/ReputationBootstrapper/Selector.tsx
+++ src/components/pages/ReputationBootstrapper/Selector.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import LogoAndText from 'components/common/LogoAndText'
+import LogoAndText from '../../common/LogoAndText'
 import Tooltip from 'components/common/Tooltip'
 import EthFinexLogo from 'assets/pngs/NECwithoutText.png'
 import StarIcon from 'assets/svgs/star.svg'
 import styled from 'styled-components'
 import { lockNEC, bidGEN, airdrop } from 'config.json'
+import { Title } from "../../../components/common/beehive/Title";
+
 import { tooltip } from 'strings'
 const HeaderWrapper = styled.div`
   display: flex;
@@ -15,19 +17,9 @@ const HeaderWrapper = styled.div`
   height: ${props => props.height};
 `
 
-const Title = styled.div`
-  display: flex;
-  flex-direction: row;
-  color: var(--white-text);
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 20px;
-  text-align: center;
-  margin: 32px 0px;
-  letter-spacing: 1px;
-`
+const TitleHolder = styled.div`
+text-align:center;
+margin-bottom:50px;`
 
 const NavWrapper = styled.div`
   display: flex;
@@ -87,7 +79,6 @@ const ButtonExternal = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: var(--action-button);
   font-family: Montserrat;
   font-style: normal;
   font-weight: 500;
@@ -97,6 +88,8 @@ const ButtonExternal = styled.div`
   padding: 9px 0px;
   cursor: pointer;
   width: 156px;
+  
+  border: 1px solid #E2A907;
 `
 
 const InactiveButton = styled(ActiveButton)`
@@ -155,11 +148,11 @@ const Selector = withRouter((props) => {
 
   return (
     <HeaderWrapper height={height}>
-      <Title>
-        How do you want to earn Reputation for the necDAO?
-        <Tooltip title="" content={tooltip.necDAOBasics} position="right top" />
-      </Title>
+ <TitleHolder>
+      <Title text={"Governance"} afterElement={true} />
+      </TitleHolder>
       <NavWrapper>
+       
         <ButtonExternal>
           <a
             href="https://alchemy.daostack.io/dao/0xe56b4d8d42b1c9ea7dda8a6950e3699755943de7/members/"
