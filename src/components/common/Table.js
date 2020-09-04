d80fe1e957ad11b644e61d46ed7eb25714b30a78
unknown <bennett_noah11@yahoo.com>
2020-09-03 23:14:47 -0400

    govern table fix

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -6,7 +6,7 @@ export const TableWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 220px;
+  height: 360px;
   overflow: auto;
 background: rgba(40, 50, 74, 0.5);
 
